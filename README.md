d8dd222a29be8fb534d08c9965019df1ad1a33cd
daniel <dasdo1@gmail.com>
2016-05-26 15:02:37 -0400

    add documentation and first releace

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# phalcon-word-keys-templates
+# PhalconDocs
 Library for fast handling of template with phpword
+
+## Requirements
+
+* PHP >= 5.3.9
+* Phalcon >= 2.0.0
+
+## Installing via Composer
+
+Install composer in a common location or in your project:
+
+```bash
+curl -s http://getcomposer.org/installer | php
+```
+
+Create the composer.json file as follows:
+
+```json
+{
+    "require": {
+        "Mctekk/PhalconDocs":"dev-master"
+    }
+}
+```
+
+## Usage
+
+TemplateProcessor
+
+```php
+<?php
+	$model = YorModels::find(1);
+	$path = '/path/to/you/template/';
+	$docs = new PhalconDocs\TemplateProcessor($path . "file.docx", $model);
+    $docs->saveAs($path . "newfile.docx");
+
+```
--- src/TemplateProcessor.php
+++ src/TemplateProcessor.php
@@ -5,14 +5,38 @@ use Phalcon\Mvc\Model;
 use PhpOffice\PhpWord\TemplateProcessor as TProcessor;
 
 /**
- *
+ *  Class to handle the management of phpdocx template for the keys with sumplantar model and relationships Phalcon
+ *  Clase para manejar los template de phpdocx para sumplantar las keys con un modelo de phalcon y sus relaciones
+ *  @author Daniel Sánchez <dasdo1@mctekk.com>
+ *  @version 1.0.0
  */
 class TemplateProcessor extends Injectable
 {
+    /**
+     * Path template file
+     * @var string
+     * @required true
+     */
     public $templatePath;
+
+    /**
+     * Phalcon models to map
+     * @var Phalcon\Mvc\Model
+     */
     public $models;
+
+    /**
+     * array keys and values outside the model
+     * @var array
+     */
     public $extras;
 
+    /**
+     * [__construct description]
+     * @param string             $templatePath Path template file
+     * @param Phalcon\Mvc\Model  $models       Phalcon models to map
+     * @param array              $extras       array keys and values outside the model
+     */
     public function __construct($templatePath, Model $models, array $extras = [])
     {
         $this->templatePath = $templatePath;
@@ -20,50 +44,234 @@ class TemplateProcessor extends Injectable
         $this->extras = $extras;
     }
 
-    public function make($name = null, $newPath = null)
+    /**
+     * Method to create a new document based on the template
+     * @param  string $name    path for the new file (path/to/file.docx)
+     * @return void
+     */
+    public function saveAs($path = null)
     {
-        $path = "";
-        if (is_null($name)) {
-            $path = "unknown.docx";
-        } else {
-            $path = is_null($newPath) ? $name : $newPath . $name;
-        }
+        $path = is_null($path) ? "unknown.docx" : $path;
+
+        /**
+         * new instance of PhpOffice\PhpWord\TemplateProcessor
+         * @var PhpOffice\PhpWord\TemplateProcessor
+         */
         $templateProcessor = new TProcessor($this->templatePath);
 
+        /**
+         * Extra parameters change first
+         */
         if (count($this->extras)) {
             foreach ($this->extras as $key => $value) {
+                /**
+                 * set value extras
+                 */
                 $templateProcessor->setValue($key, $value);
             }
         }
 
-        $values = $this->getValues();
+        /**
+         * get models values formated
+         * @var array
+         */
+        $modelsValues = $this->getModelsValues();
 
-        foreach ($values as $key => $value) {
-            $templateProcessor->setValue($key, $value);
+        /**
+         * We iterate the values obtained to apply the values to the templates
+         */
+        foreach ($modelsValues as $key => $value) {
+            /**
+             * If an array has a different management to flat values
+             */
+            if (is_array($value)) {
+                try {
+                    /**
+                     * Clone rows based on the primary key of the relationship model
+                     */
+                    $templateProcessor->cloneRow($key, count($value));
+
+                    foreach ($value as $k => $next) {
+                        /**
+                         * Set a clone value
+                         */
+                        foreach ($next as $ke => $v) {
+                            $ke = $key . "." . $ke;
+                            $templateProcessor->setValue($ke, $v);
+                        }
+                        /**
+                         * remove unnecessary keys
+                         */
+                        $r = $key . "#" . ($k + 1);
+                        $templateProcessor->setValue($r, '');
+                    }
+                } catch (\Exception $e) {
+                    /**
+                     * If a relationship of many is not used the library throws an exception key not found, continue with the capture and execution.
+                     * Si una relacion de muchos no es usado la libreria arroja una excepcion de llave no encontrada, la capturamos y seguimos con la ejecucion.
+                     */
+                    continue;
+                }
+            } else {
+                $templateProcessor->setValue($key, $value);
+            }
         }
 
-        return $templateProcessor->saveAs($path);
+        /**
+         * Save the new file
+         */
+        $templateProcessor->saveAs($path);
+        return;
     }
 
-    public function getValues()
+    /**
+     * Method for relationships and values model
+     * @return Array [description]
+     */
+    public function getModelsValues()
     {
         $values = [];
+
         if (!$this->models) {
             throw new Exception("no data model", 1);
         }
 
-        if ($base = $this->toArray()) {
-            foreach ($variable as $key => $value) {
+        /**
+         * get class name
+         * @var string
+         */
+        $namespaceModelsName = get_class($this->models);
+        
+        /**
+         * avoid php warning
+         * @var array
+         */
+        $explode = explode("\\", $namespaceModelsName);
+        
+        /**
+         * get base models name for the key
+         * @var string
+         */
+        $modelName = strtolower(end($explode));
+
+        if ($base = $this->models->toArray()) {
+            foreach ($base as $key => $value) {
+                $key = $modelName . "." . $key;
+                /**
+                 * based information model
+                 * example.value = value
+                 */
                 $values[$key] = $value;
             }
         } else {
             throw new Exception("model empty", 1);
         }
 
-        foreach ($this->modelsManager->getRelations(get_class($this->models)) as $r) {
+        /**
+         * we obtain relations model
+         */
+        foreach ($this->modelsManager->getRelations($namespaceModelsName) as $r) {
+            /**
+             * relations internal information model
+             * @var array
+             */
             $options = $r->getOptions();
-            $relacion[$r->getFields()] = $options["alias"];
+
+            /**
+             * Name of relationship
+             * @var string
+             */
+            $relationshipName = $options["alias"];
+
+            /**
+             * relationship information
+             * @var Phalcon\Mvc\Model Or Phalcon\Mvc\Model\Resultset\Simple
+             */
+            if ($relationship = $this->models->{$relationshipName}) {
+                foreach ($relationship->toArray() as $k => $value) {
+                    $key = $modelName . "." . $relationshipName;
+                    if (is_array($value)) {
+                        foreach ($value as $indice => $valor) {
+                            $values[$key][$k][$indice . '#' . ($k + 1)] = $valor;
+                        }
+                    } else {
+                        $values[$key . "." . $k] = $value;
+                    }
+                }
+            }
         }
-        print_r($relacion);
+        return $values;
+    }
+
+    /**
+     * Gets the Path template file.
+     *
+     * @return string
+     */
+    public function getTemplatePath()
+    {
+        return $this->templatePath;
+    }
+
+    /**
+     * Sets the Path template file.
+     *
+     * @param string $templatePath the template path
+     *
+     * @return self
+     */
+    public function setTemplatePath($templatePath)
+    {
+        $this->templatePath = $templatePath;
+
+        return $this;
+    }
+
+    /**
+     * Gets the Phalcon models to map.
+     *
+     * @return Phalcon\Mvc\Model
+     */
+    public function getModels()
+    {
+        return $this->models;
+    }
+
+    /**
+     * Sets the Phalcon models to map.
+     *
+     * @param Phalcon\Mvc\Model $models the models
+     *
+     * @return self
+     */
+    public function setModels(Model $models)
+    {
+        $this->models = $models;
+
+        return $this;
+    }
+
+    /**
+     * Gets the array keys and values outside the model.
+     *
+     * @return array
+     */
+    public function getExtras()
+    {
+        return $this->extras;
+    }
+
+    /**
+     * Sets the array keys and values outside the model.
+     *
+     * @param array $extras the extras
+     *
+     * @return self
+     */
+    public function setExtras(array $extras)
+    {
+        $this->extras = $extras;
+
+        return $this;
     }
 }
